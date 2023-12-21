f29aa8cd2f86b48f1fa6b039dff4e7dba840e8d2
suha <75898031+hp0724@users.noreply.github.com>
2023-12-21 12:27:16 +0900

    Title: 정수 명령 처리 5, Time: 180ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231221/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList arr = new ArrayList();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer str;
+        StringBuilder sb = new StringBuilder();
+
+        while (N-->0){
+            str = new StringTokenizer(br.readLine(), " ");
+
+            int strLen = str.countTokens();
+
+            if (strLen > 1){
+                String str1 = str.nextToken();
+                int str2 = Integer.parseInt(str.nextToken());
+
+                if (str1.equals("push_back")){
+                    arr.add(str2);
+                } else {
+                    sb.append(arr.get(str2-1)).append("\n");
+                }
+            } else {
+                String str1 = str.nextToken();
+                if (str1.equals("pop_back")){
+                    arr.remove(arr.size()-1);
+
+                } else if (str1.equals("size")){
+                    sb.append(arr.size()).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+        
+    }
+}
