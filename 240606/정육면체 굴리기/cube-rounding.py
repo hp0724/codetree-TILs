f575f482854056803851afaad40c4e5b950f77ab
suha <75898031+hp0724@users.noreply.github.com>
2024-06-06 00:36:31 +0900

    Title: 정육면체 굴리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240606/정육면체 굴리기/cube-rounding.py
@@ -0,0 +1,73 @@
+# 0 ~9 까지의 임의의 숫자 
+# n * m 격자판에 한 면이 1 * 1 크기인 정육면체 
+# n , m , x, y , k 
+# 지도 
+# 굴리기 방향 
+
+import sys
+input = sys.stdin.readline
+
+# 동 서 북 남 
+dx = [0,1,-1,0,0]
+dy = [0,0,0,-1,1]
+
+dice = [0,0,0,0,0,0]
+
+n,m,x,y,k = map(int,input().split())
+
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+direction = list(map(int,input().split()))
+if graph[y][x] == 0:
+    graph[y][x] = dice[5]
+    # 칸에 쓰여져 있는 수가 0이 아니면 , 칸에 쓰여져있는 수가 바닥면으로 복사 
+    # 해당 칸의 수는 0 
+elif graph[y][x] != 0:
+    dice[5] = graph[y][x]
+    graph[y][x] = 0
+
+for d in direction:
+    nx = x + dx[d]
+    ny = y + dy[d]
+    #print("before",dice)
+    # 동쪽 dice = [ 5,2,1,4,6,3]
+    # 서쪽 dice = [3,2,1,4,6,5]
+    # 남쪽 dice = [4,1,3,6,5,2]
+    # 북쪽 dice = [2,1,3,6,5,4]
+    if nx < 0 or nx >= n or ny < 0 or ny >= m :
+        continue
+    x = nx 
+    y = ny 
+    
+    
+
+    #print(graph[y][x])
+    
+    
+    # 동쪽
+    if d == 1:
+        dice[0],dice[1],dice[2],dice[3],dice[4],dice[5] = dice[4],dice[1],dice[0],dice[3],dice[5],dice[2] 
+    # 서쪽
+    elif d == 2:
+        dice[0],dice[1],dice[2],dice[3],dice[4],dice[5] = dice[2],dice[1],dice[5],dice[3],dice[0],dice[4] 
+    # 북쪽
+    elif d == 3:
+        dice[0],dice[1],dice[2],dice[3],dice[4],dice[5] = dice[1],dice[5],dice[2],dice[0],dice[4],dice[3] 
+    # 남쪽 
+    elif d == 4:
+        dice[0],dice[1],dice[2],dice[3],dice[4],dice[5] = dice[3],dice[0],dice[2],dice[5],dice[4],dice[1]     
+    
+    # 칸에 쓰여져 있는 수가 0이면 , 주사위의 바닥에 쓰여져있는 수가 칸에 복사 
+    if graph[y][x] == 0:
+        graph[y][x] = dice[5]
+    # 칸에 쓰여져 있는 수가 0이 아니면 , 칸에 쓰여져있는 수가 바닥면으로 복사 
+    # 해당 칸의 수는 0 
+    elif graph[y][x] != 0:
+        dice[5] = graph[y][x]
+        graph[y][x] = 0
+
+    #print("after",dice)
+    print(dice[0])
