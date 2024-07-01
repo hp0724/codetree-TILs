3f6b8ae401337b71322d97785eed689841641e21
suha <75898031+hp0724@users.noreply.github.com>
2024-07-01 15:05:28 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/요일 맞추기/guess-day-of-week.py
+++ 240701/요일 맞추기/guess-day-of-week.py
@@ -3,8 +3,8 @@ m1,d1,m2,d2 = map(int,input().split())
 month_day = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 day_string = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"]
 
-diff = (month_day[m2] + d2) - (month_day[m1] + d1)
-
+diff = (sum(month_day[0:m2]) + d2) - (sum(month_day[0:m1]) + d1)
+# print(diff)
 if diff < 0 :
     while diff > 0:
         diff += 7 
