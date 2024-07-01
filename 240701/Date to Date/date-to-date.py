6f8bd1f55ea03a701ab713fad1e9367a7dc3d93a
suha <75898031+hp0724@users.noreply.github.com>
2024-07-01 14:35:42 +0900

    Title: Date to Date, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240701/Date to Date/date-to-date.py
+++ 240701/Date to Date/date-to-date.py
@@ -1,15 +1,18 @@
-m1,d1,m2,d2 = map(int,input().split())
+before_month,before_day,after_month,after_day = map(int,input().split())
  
-elapsed_days = 0 
-
+elapsed_days = 1 
+#                1  2  3  4  5  6  7  8  9  10 11 12 
 num_of_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 while True:
-    if month == m2 and day == d2:
+    if before_month == after_month and before_day == after_day:
         break 
     
     elapsed_days += 1 
-    d1 += 1 
-    if day > num_of_days[month]:
-        m1 += 1 
-        d1 = 1
+    before_day += 1 
+
+    if before_day > num_of_days[before_month]:
+        before_month += 1 
+        before_day = 1
+
+print(elapsed_days)
