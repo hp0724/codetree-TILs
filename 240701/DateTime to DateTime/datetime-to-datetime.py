8ca9832c48131494b6fd6cc62eeca4e209901165
suha <75898031+hp0724@users.noreply.github.com>
2024-07-01 14:40:42 +0900

    Title: DateTime to DateTime, Time: 5073ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240701/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,21 @@
+a,b,c = map(int,input().split())
+if a<=11 and b<=11 and c<11 :
+    print(-1)
+before_day = 11
+before_hour = 11
+before_min = 11 
+time = 0
+while True:
+    if before_day == a and before_hour == b and before_min == c :
+        break 
+    time +=1 
+    before_min += 1 
+    if before_min > 60:
+        before_min = 1
+        before_hour +=1 
+    
+    elif before_hour> 23:
+        before_day += 1
+        before_hour = 0
+
+print(time )
