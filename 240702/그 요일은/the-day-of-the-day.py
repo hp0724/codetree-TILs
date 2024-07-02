9f4b0c5b5d134a00f50b1cd391a92dd700448089
suha <75898031+hp0724@users.noreply.github.com>
2024-07-02 22:21:48 +0900

    Title: 그 요일은, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240702/그 요일은/the-day-of-the-day.py
+++ 240702/그 요일은/the-day-of-the-day.py
@@ -9,9 +9,12 @@ for idx,value in enumerate(day_of_the_week):
 
 # 월요일 부터 입력 요일 까지 값 빼고 
 # 일주일 나누기 
-
+count = 0
 sum_day = (sum(month_day[:m2]) + d2) - (sum(month_day[:m1]) + d1)
-sum_day -= idx 
-result = sum_day // 7
+if idx != 0 and sum_day - idx > 0:
+    sum_day -= idx 
+    count += 1
+    if sum_day >= 7:
+        count += sum_day // 7
 
-print(result+1)
+print(count)
