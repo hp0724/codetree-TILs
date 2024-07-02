263445e7a000680684e0c9063be15a0af6241bcb
suha <75898031+hp0724@users.noreply.github.com>
2024-07-02 22:43:09 +0900

    Title: 그 요일은, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240702/그 요일은/the-day-of-the-day.py
+++ 240702/그 요일은/the-day-of-the-day.py
@@ -3,18 +3,17 @@ day_of_the_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 m1,d1,m2,d2 = map(int,input().split())
 input_day_of_the_week = input()
 
-for idx,value in enumerate(day_of_the_week):
-    if input_day_of_the_week == value:
-        result = idx 
+result_idx = day_of_the_week.index(input_day_of_the_week)
+print(result_idx)
 
-# 월요일 부터 입력 요일 까지 값 빼고 
-# 일주일 나누기 
-count = 0
-diff = (sum(month_day[:m2]) + d2) - (sum(month_day[:m1]) + d1)
-if diff - result >= 0:
-    diff -= result 
-    count += 1
-    if diff >= 7:
-        count += (diff // 7)
+# # 월요일 부터 입력 요일 까지 값 빼고 
+# # 일주일 나누기 
+# count = 0
+# diff = (sum(month_day[:m2]) + d2) - (sum(month_day[:m1]) + d1)
+# if diff - result_idx >= 0:
+#     diff -= result_idx 
+#     count += 1
+#     if diff >= 7:
+#         count += (diff // 7)
 
-print(count)
+# print(count)
