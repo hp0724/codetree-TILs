69011f19d2449f50b4d08aea32db9471e387d9b3
suha <75898031+hp0724@users.noreply.github.com>
2024-07-03 14:54:10 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240703/연속되는 수 2/continuous-number2.py
+++ 240703/연속되는 수 2/continuous-number2.py
@@ -2,20 +2,19 @@ n = int(input())
 result_array = []
 cnt = 1
 idx = 0
-a = []
+arr = []
 for _ in range(n):
-    a.append(int(input()))
-
-for i in range(1,n):
-    if n == 1:
-        result_array.append(cnt)
-    if a[i] == a[i-1]:
+    arr.append(int(input()))
+for i in range(n):
+    if len(arr) == 1:
+        result_array.append(arr[0])
+        break
+    elif arr[i] == arr[i-1]:
         cnt+=1 
     else:
         result_array.append(cnt)
         cnt = 0
-    # print(a)
-    # print(result_array)
-
-
-print(max(result_array)+1)
+if len(result_array) == 1:
+    print(1)
+else:
+    print(max(result_array)+1)
