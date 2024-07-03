ac3f24d7b4587cf942794eab43338cea7ca60af2
suha <75898031+hp0724@users.noreply.github.com>
2024-07-03 11:14:16 +0900

    Title: 최대로 겹치는 지점, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240703/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,10 @@
+n = int(input())
+MAX_VALUE = 101
+OFFSET = 100
+array = [0]*(MAX_VALUE+OFFSET)
+for _ in range(n):
+    x1,x2 = map(int,input().split())
+    for i in range(x1+OFFSET,x2+OFFSET+1):
+        array[i] += 1 
+
+print(max(array))
