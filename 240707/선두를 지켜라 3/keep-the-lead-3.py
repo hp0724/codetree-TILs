e6022fa6cc49de3b3607bf14805f4f23bbac3bc1
suha <75898031+hp0724@users.noreply.github.com>
2024-07-07 14:47:50 +0900

    Title: 선두를 지켜라 3, Time: 197ms, Memory: 45MB, Status: Wrong Answer - CodeTree

---
+++ 240707/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,30 @@
+N,M = map(int,input().split())
+
+a_arr = [0]
+b_arr = [0]
+for _ in range(N):
+    v,t = map(int,input().split())
+    for _ in range(t):
+        a_arr.append(a_arr[-1]+v)
+
+for _ in range(M):
+    v,t = map(int,input().split())
+    for _ in range(t):
+        b_arr.append(b_arr[-1]+v)
+
+# print(a_arr)
+# print(b_arr)
+
+change_cnt = 0
+winner = ""
+for i in range(len(a_arr)):
+    if a_arr[i] >= b_arr[i] and winner!="a"  :
+        change_cnt += 1 
+        winner = "a"
+    elif b_arr[i] >= a_arr[i] and winner!="b" :
+        change_cnt += 1
+        winner = "b"
+if change_cnt == 0 :
+    print(0)
+else:
+    print(change_cnt-1)
