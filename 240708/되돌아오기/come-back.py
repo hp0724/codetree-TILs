cb5fc6a5a7b2f7c2d32c6e218a7b7f935d84d463
suha <75898031+hp0724@users.noreply.github.com>
2024-07-08 13:23:20 +0900

    Title: 되돌아오기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/되돌아오기/come-back.py
@@ -0,0 +1,29 @@
+x = 0
+y = 0
+
+N = int(input())
+
+mapper ={
+    "N": 0,
+    "E": 1,
+    "W": 2,
+    "S": 3
+}
+
+dx = [-1,0,0,1]
+dy = [0,1,-1,0]
+
+cnt = 0
+for _ in range(N):
+    d,c = input().split()
+    c = int(c)
+    for _ in range(c):
+        x += dx[mapper[d]]
+        y += dy[mapper[d]]
+        cnt += 1 
+        if x == 0 and y == 0 :
+            break 
+
+    if x== 0 and y == 0:
+        break
+print(cnt)
