112c0177d5777b0ea944c3d6be6c2397f60497e6
suha <75898031+hp0724@users.noreply.github.com>
2024-08-19 21:50:01 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240819/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,21 @@
+# 1번 체크포인트 -> N번 체크포인트에서 끝나기 
+# 1번하고 N번 빼고 체크포인트 건너뛰기 1개 가능 
+# 맨하튼 거리로 계산 
+
+n = int(input())
+arr = []
+result = 1e9
+temp_result = 0
+for _ in range(n):
+    x,y = map(int,input().split())
+    arr.append((x,y))
+
+for i in range(1,n-1):
+    temp = arr.pop(i)
+    for j in range(len(arr)-1):
+        x1,y1 = arr[j]
+        x2,y2 = arr[j+1]
+        temp_result += (abs(x2-x1) + abs(y2-y1))
+    result = min(result,temp_result)
+    arr.insert(i,temp)
+print(result)
