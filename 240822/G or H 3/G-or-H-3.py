d798ccf20963cdbb607e01c281373d65706f9cac
suha <75898031+hp0724@users.noreply.github.com>
2024-08-22 00:39:19 +0900

    Title: G or H 3, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/G or H 3/G-or-H-3.py
@@ -0,0 +1,27 @@
+N,K = map(int,input().split()) 
+max_value = 0
+arr = []
+for _ in range(N):
+    idx,alpha = input().split()
+    max_value = max(int(idx),max_value)
+    arr.append([idx,alpha])
+position = [0] * (max_value + 1)
+
+for a in arr:
+    position_idx = a[0]
+    position_idx = int(position_idx)
+    position_alphabet = a[1]
+
+    if position_alphabet == "G":
+        position[position_idx] = 1
+    elif position_alphabet == 'H':
+        position[position_idx] = 2
+result = 0
+# 16 - 6 = 10
+for i in range(0,len(position)-K):
+    temp = 0
+    for j in range(i,i+K+1):
+        temp += position[j]
+    result = max(result,temp)
+
+print(result)
