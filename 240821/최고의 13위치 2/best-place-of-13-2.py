ef1da219ca34edc0ed48c9b916ac5e70edac76b8
suha <75898031+hp0724@users.noreply.github.com>
2024-08-21 21:00:34 +0900

    Title: 최고의 13위치 2, Time: 147ms, Memory: 26MB, Status: Passed - CodeTree

--- 240821/최고의 13위치 2/best-place-of-13-2.py
+++ 240821/최고의 13위치 2/best-place-of-13-2.py
@@ -6,8 +6,12 @@ for _ in range(n):
 ans = 0 
 for i in range(n):
     for j in range(n-2):
-        for k in range(i+1,n):
+        for k in range(n):
             for l in range(n-2):
-                ans = max(ans,(arr[i][j] + arr[i][j+1] + arr[i][j+2]) + (arr[k][l] + arr[k][l+1] + arr[k][l+2]))
+                if i== k and abs(j-l) <=2 :
+                    continue
+                sum_value = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                sum_value2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                ans = max(ans,sum_value + sum_value2)
 
 print(ans)
