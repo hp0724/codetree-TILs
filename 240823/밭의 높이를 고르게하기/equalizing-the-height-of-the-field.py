0ad8383a49d59bd58e4b0ae8a0ac0dac9f355970
suha <75898031+hp0724@users.noreply.github.com>
2024-08-23 10:54:36 +0900

    Title: 밭의 높이를 고르게하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240823/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,18 +1,13 @@
-def min_cost_to_equalize_height(N, H, T, heights):
-    min_cost = float('inf')
-
-    for i in range(N - T + 1):
-        cost = 0
-        for j in range(T):
-            cost += abs(heights[i + j] - H)
-        min_cost = min(min_cost, cost)
-
-    return min_cost
-
 # 입력 예제
 N, H, T = map(int, input().split())
 heights = list(map(int, input().split()))
 
-# 최소 비용 계산
-result = min_cost_to_equalize_height(N, H, T, heights)
-print(result)
+min_cost = 1e6
+
+for i in range(N - T + 1):
+    cost = 0
+    for j in range(T):
+        cost += abs(heights[i + j] - H)
+    min_cost = min(min_cost, cost)
+
+print(min_cost)
