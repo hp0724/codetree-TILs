5d46bc510d9870e65f1c8b2171f0ce1eafd7d38b
suha <75898031+hp0724@users.noreply.github.com>
2024-08-23 11:13:08 +0900

    Title: 밭의 높이를 고르게하기, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240823/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240823/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,13 +1,14 @@
-# 입력 예제
-N, H, T = map(int, input().split())
-arr = list(map(int, input().split()))
+#  N개의 밭의 높이가 주어지면 연속하게 최소 T번 이상 H높이로 나오게끔 하려고 할 때
+N,H,T = map(int,input().split())
 
-min_cost = 1e6
+arr = list(map(int,input().split()))
 
-for i in range(N - T + 1):
-    cost = 0
-    for j in range(T):
-        cost += abs(arr[i + j] - H)
-    min_cost = min(min_cost, cost)
+min_value = 1e6
+for i in range(N):
+    for j in range(i+T,N):
+        temp = 0
+        for k in range(i,j):
+            temp += abs(H-arr[k])
+        min_value = min(min_value,temp)
 
-print(min_cost)
+print(min_value)
