75672df04977c4205ee477fce0ae7fa3e6b747a7
suha <75898031+hp0724@users.noreply.github.com>
2024-08-27 14:04:50 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 111ms, Memory: 28MB, Status: Passed - CodeTree

--- 240827/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240827/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -1,17 +1,16 @@
 import sys 
 n = int(input())
 
-
 arr = []
 for _ in range(n):
-    x,y = map(int,input().split())
-    arr.append((x,y))
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
 min_value = sys.maxsize
+
 for i in range(n):
-    for j in range(n):
-        if j == i:
-            continue 
-        temp = (arr[i][0] -arr[j][0])* (arr[i][0] -arr[j][0])  + (arr[i][1] -arr[j][1]) *  (arr[i][1] -arr[j][1])
-    min_value = min(min_value,temp)
+    for j in range(i + 1, n):
+        temp = (arr[i][0] - arr[j][0]) ** 2 + (arr[i][1] - arr[j][1]) ** 2
+        min_value = min(min_value, temp)
 
 print(min_value)
