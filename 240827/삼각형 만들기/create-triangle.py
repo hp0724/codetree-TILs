8b0957744671ea71b405e464ed3d38586a145aac
suha <75898031+hp0724@users.noreply.github.com>
2024-08-27 15:02:51 +0900

    Title: 삼각형 만들기, Time: 190ms, Memory: 26MB, Status: Passed - CodeTree

--- 240827/삼각형 만들기/create-triangle.py
+++ 240827/삼각형 만들기/create-triangle.py
@@ -6,17 +6,36 @@ for _ in range(n):
     x,y = map(int,input().split())
     arr.append((x,y))
 
-result = 0 
+max_result = 0 
 # x축에 평행하거나 y축에 평행한경우 
 # (0,0), (1,0), (1,2)
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            if arr[i][1] - arr[j][1] == 0 and arr[j][0] - arr[k][0] == 0 :
-                result = abs(arr[i][0] - arr[j][0]) * abs(arr[j][1] - arr[k][1])
-            if arr[i][1] - arr[k][1] == 0 and arr[k][0] - arr[j][0] == 0: 
-                result = abs(arr[i][0] - arr[k][0]) * abs(arr[k][1] - arr[j][1])
-            if arr[j][1] - arr[k][1] == 0 and arr[k][0] - arr[i][0] == 0: 
-                result = abs(arr[j][0] - arr[k][0]) * abs(arr[k][1] - arr[i][1])
+            result = 0
+            if arr[i][0] - arr[j][0] == 0 and arr[j][1] - arr[k][1] == 0 :
+                result = abs(arr[i][1] - arr[j][1]) * abs(arr[j][0] - arr[k][0])
+                max_result = max(max_result,result)
 
-print(result)
+            if arr[i][0] - arr[k][0] == 0 and arr[k][1] - arr[j][1] == 0 :
+                result = abs(arr[i][1] - arr[k][1]) * abs(arr[k][0] - arr[j][0])
+                max_result = max(max_result,result)
+
+            if arr[j][0] - arr[i][0] == 0 and arr[i][1] - arr[k][1] == 0 :
+                result = abs(arr[j][1] - arr[i][1]) * abs(arr[i][0] - arr[k][0])
+                max_result = max(max_result,result)
+
+            if arr[j][0] - arr[k][0] == 0 and arr[k][1] - arr[i][1] == 0 :
+                result = abs(arr[j][1] - arr[k][1]) * abs(arr[k][0] - arr[i][0])
+                max_result = max(max_result,result)
+
+            if arr[k][0] - arr[i][0] == 0 and arr[i][1] - arr[j][1] == 0 :
+                result = abs(arr[k][1] - arr[i][1]) * abs(arr[i][0] - arr[j][0])
+                max_result = max(max_result,result)
+
+            if arr[k][0] - arr[j][0] == 0 and arr[j][1] - arr[i][1] == 0 :
+                result = abs(arr[k][1] - arr[j][1]) * abs(arr[j][0] - arr[i][0])
+                max_result = max(max_result,result)
+
+
+print(max_result)
